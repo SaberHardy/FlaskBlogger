08e7d06521313838a0023a3ca8b346018590a5fc
SaberHardy <samirsamado5@gmail.com>
2022-12-16 21:55:18 +0100

    first configurations pages

--- app.py
+++ app.py
@@ -1,11 +1,26 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def home():  # put application's code here
+    return render_template('home.html')
+
+
+@app.route('/user/<name>/')
+def user(name):  # put application's code here
+    return render_template('user.html', name=name)
+
+
+@app.errorhandler(404)
+def custom_err(err):
+    return render_template('error404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(err):
+    return render_template('error505.html'), 500
 
 
 if __name__ == '__main__':
