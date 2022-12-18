ad881cf598f60e8d942f0954cf9f0a565a36f02d
SaberHardy <samirsamado5@gmail.com>
2022-12-18 21:35:13 +0100

    add user

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, flash
-
-from forms import NameForm
+from forms import NameForm, UserForm
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
@@ -12,6 +11,18 @@ app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///users.db"
 app.config['SECRET_KEY'] = "my secret key to world"
 
 db = SQLAlchemy(app)
+
+
+class Users(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(200), nullable=False)
+    email = db.Column(db.String(200), nullable=False, unique=True)
+    date_added = db.Column(db.DateTime, default=datetime.utcnow)
+
+    # Create string
+
+    def __repr__(self):
+        return '<Name %r>' % self.name
 
 
 @app.route('/')
@@ -47,6 +58,28 @@ def name():
     return render_template('name.html',
                            name=name,
                            form=form)
+
+
+@app.route('/user/add', methods=['GET', 'POST'])
+def add_user():
+    name = None
+    form = UserForm()
+    if form.validate_on_submit():
+        user = Users.query.filter_by(email=form.email.data).first()
+        if user is None:
+            user = Users(name=form.name.data, email=form.email.data)
+            db.session.add(user)
+            db.session.commit()
+
+        name = form.name.data
+        form.name.data = ""
+        form.email.data = ""
+        flash("User added successfully!")
+    our_users = Users.query.order_by(Users.date_added)
+    return render_template('add_user.html',
+                           form=form,
+                           name=name,
+                           our_users=our_users)
 
 
 if __name__ == "__main__":
--- forms.py
+++ forms.py
@@ -6,3 +6,9 @@ from wtforms.validators import DataRequired
 class NameForm(FlaskForm):
     name = StringField("What's your name sir?", validators=[DataRequired()])
     submit = SubmitField("Submit")
+
+
+class UserForm(FlaskForm):
+    name = StringField('Name', validators=[DataRequired()])
+    email = StringField('email', validators=[DataRequired()])
+    submit = SubmitField("Submit")
